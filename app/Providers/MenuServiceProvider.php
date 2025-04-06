d323416f910c1209e08a014dbc01820c5972fb0f
ntuananhdevs <anhndt25@gmail.com>
2025-04-07 02:55:46 +0700

    change layout

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use \Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class MenuServiceProvider extends ServiceProvider
@@ -30,6 +30,6 @@ class MenuServiceProvider extends ServiceProvider
         $horizontalMenuData = json_decode($horizontalMenuJson);
 
         // Share all menuData to all the views
-        \View::share('menuData',[$verticalMenuData, $horizontalMenuData]);
+        View::share('menuData', [$verticalMenuData, $horizontalMenuData]);
     }
 }
