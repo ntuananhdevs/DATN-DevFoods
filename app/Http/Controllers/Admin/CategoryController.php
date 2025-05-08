7f4d80e63890d907d054d8f045d52829eec3132b
ntuananhdevs <anhndt25@gmail.com>
2025-05-08 13:29:03 +0700

    fix controller, delete btn right

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Category;
+use App\Models\Admin\Category;
 use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
