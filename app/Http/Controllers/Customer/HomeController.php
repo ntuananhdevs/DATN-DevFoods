0bd0bd9c9945dbdc1222c050d07a7cc247e494bb
Rachel <tungxeko912@gmail.>
2025-05-08 20:13:50 +0700

    New Products Pls Run db:seed

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    // Dashboard - Analytics
+    
     public function index()
     {
-        return view("customer.home");
+        $products = Product::with('category')->get();
+        $categories = Category::withCount('products')->get();
+        return view('customer.home', compact('products', 'categories'));
     }
 }
 
