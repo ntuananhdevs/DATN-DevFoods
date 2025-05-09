8811b8c2265bfaef6648c9cf6609689198f0e3d9
pHieu1504 <haa144557@gmail.com>
2025-05-09 23:23:50 +0700

    role

--- app/Http/Middleware/RoleAdmin.php
+++ app/Http/Middleware/RoleAdmin.php
@@ -12,7 +12,7 @@ class RoleAdmin
      */
     public function handle($request, Closure $next)
     {
-        if (!Auth::check() || Auth::user()->role_id !== 17) {
+        if (!Auth::check() || Auth::user()->role_id !== 1) {
             return redirect()->route('admin.login')->with('error', 'Bạn không có quyền truy cập.');
         }
 
