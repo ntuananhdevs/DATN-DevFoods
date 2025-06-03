472eb66d091b67b4c8bba8f2addd3f2d767835c3
NgxGiap <nguyenvangiap215@gmail.com>
2025-06-04 00:34:39 +0700

    Home

--- resources/views/layouts/customer/fullLayoutMaster.blade.php
+++ resources/views/layouts/customer/fullLayoutMaster.blade.php
@@ -33,7 +33,7 @@
     </script>
     <!-- Font Awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
     <!-- Notification Styles -->
     <style>
         @keyframes slideInDown {
